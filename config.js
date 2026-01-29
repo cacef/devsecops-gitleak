9a11a95720df0d4b79599d6825b1971effb45c8d
cam <camille.andreotta2025@campus-eni.fr>
2026-01-29 09:41:10 +0100

    Add config

---
+++ config.js
@@ -0,0 +1,9 @@
+// Configuration de l’application
+const config = {
+  apiKey: "AKIAIOSFODNN7EXAMPLE",
+  secretKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+  dbPassword: "super_secret_password_123",
+  githubToken: "ghp_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+};
+
+module.exports = config;
